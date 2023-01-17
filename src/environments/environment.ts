c5a615d38101df7a0e6de47c8b97266bd9de6ba1
eanton71 <eanton71@gmail.com>
2023-01-17 18:00:47 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,13 @@ export const environment = {
   url_login:'http://localhost:3000/api/login-user',
   url_register:'http://localhost:3000/api/register-user',
   url_update_image:'http://localhost:3000/api/upload/user-pic',
-  pubkey:``
+  pubkey:`-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2jJ+qO9VA3AmvOBpPThd
+A62hUcRsZP65s0SE+4Z0E5SEgQZJxeeMmEnuS0ra/sPEyY7Uivtk4t6qCw4qtXpB
+zCWpTxBjFP62tRmA1Ou0jsVPVyye7HBhXD0CmufEODBWOt758O4vF9BESIFf46EG
+1GJvjK0qc1mXqEVfPtM+X6+II2GCXPofpsWCoLp7COJd73KgWL9U8YgjLkPtpCAj
+dvgWbqupgeUOKj0vt73/bYiYAWtAP6CEz6q8cm4gf7wsaz/a7lMjvENjeO6EWZRg
+tx2Ajkc4d5XnnbOyspFakxh8eJo9bm0/XvtXVf5PhGxAusU1SrINVBtDU7QCOGpz
+gQIDAQAB
+-----END PUBLIC KEY-----`
 };
